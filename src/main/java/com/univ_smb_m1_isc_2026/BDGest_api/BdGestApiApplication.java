c9f8c15447facd381a5d2f4730f44b1984fde321
LudovicFS <l.frealsaison@gmail.com>
2026-02-27 16:53:41 +0100

    init

---
+++ src/main/java/com/univ_smb_m1_isc_2026/BDGest_api/BdGestApiApplication.java
@@ -0,0 +1,13 @@
+package com.univ_smb_m1_isc_2026.BDGest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdGestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdGestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univ_smb_m1_isc_2026/BDGest_api/BdGestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univ_smb_m1_isc_2026.BDGest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdGestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
