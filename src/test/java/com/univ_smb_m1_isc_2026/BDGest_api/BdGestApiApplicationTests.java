2ddd8c388d2fdf9bc477db0ee437931f72b51d83
LudovicFS <l.frealsaison@gmail.com>
2026-03-28 11:36:45 +0100

    disabled tests

--- src/test/java/com/univ_smb_m1_isc_2026/BDGest_api/BdGestApiApplicationTests.java
+++ src/test/java/com/univ_smb_m1_isc_2026/BDGest_api/BdGestApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.univ_smb_m1_isc_2026.BDGest_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Disabled;
 
 @SpringBootTest
+@Disabled("Nécessite une base de données PostgreSQL active — lancer docker compose up avant d'activer ce test")
 class BdGestApiApplicationTests {
 
 	@Test
