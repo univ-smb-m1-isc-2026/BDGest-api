a7bbcd03d8fa20b4e3a612bf920250bd5e85f419
LudovicFS <l.frealsaison@gmail.com>
2026-03-29 18:56:33 +0200

    fix cors error

--- src/main/java/com/univ_smb_m1_isc_2026/BDGest_api/config/SecurityConfig.java
+++ src/main/java/com/univ_smb_m1_isc_2026/BDGest_api/config/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable()) // pour API REST
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll() // 🔓 tout autoriser
